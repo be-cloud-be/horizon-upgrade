8625217c919520aaca6e30b6caea561252a3ee3e
Jérôme Sonnet <jerome.sonnet@imply.lu>
2021-11-28 08:42:26 +0100

    Add dooba 9 dockerfile as jessie is getting old ;-)

--- odoo/custom/entrypoint.d/50-clone-old-assets.py
+++ odoo/custom/entrypoint.d/50-clone-old-assets.py
@@ -4,6 +4,48 @@ import os
 import requests
 import subprocess
 
+try:
+    from subprocess import CompletedProcess
+except ImportError:
+    # Python 2
+
+
+    class CompletedProcess:
+
+        def __init__(self, args, returncode, stdout=None, stderr=None):
+            self.args = args
+            self.returncode = returncode
+            self.stdout = stdout
+            self.stderr = stderr
+
+        def check_returncode(self):
+            if self.returncode != 0:
+                err = subprocess.CalledProcessError(self.returncode, self.args, output=self.stdout)
+                raise err
+            return self.returncode
+
+    def sp_run(*popenargs, **kwargs):
+        input = kwargs.pop("input", None)
+        check = kwargs.pop("handle", False)
+        if input is not None:
+            if 'stdin' in kwargs:
+                raise ValueError('stdin and input arguments may not both be used.')
+            kwargs['stdin'] = subprocess.PIPE
+        process = subprocess.Popen(*popenargs, **kwargs)
+        try:
+            outs, errs = process.communicate(input)
+        except:
+            process.kill()
+            process.wait()
+            raise
+        returncode = process.poll()
+        if check and returncode:
+            raise subprocess.CalledProcessError(returncode, popenargs, output=outs)
+        return CompletedProcess(popenargs, returncode, stdout=outs, stderr=errs)
+
+    subprocess.run = sp_run
+    # ^ This monkey patch allows it work on Python 2 or 3 the same way
+
 from doodbalib import ADDONS_DIR, ADDONS_YAML, SRC_DIR, addons_config, logger
 
 from io import BytesIO
