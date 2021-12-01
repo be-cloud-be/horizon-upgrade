57222d6bc08bd905b3eee270fa2c97bc3a5fe762
Jérôme Sonnet <jerome.sonnet@imply.lu>
2021-12-01 16:03:47 +0100

    fix

--- odoo/custom/src/private/after_upgrade.py
+++ odoo/custom/src/private/after_upgrade.py
@@ -15,6 +15,10 @@ def main(env):
     env['ir.module.module'].search([('name', '=', 'web_widget_timepicker')]).button_immediate_uninstall()
     
     env['ir.module.module'].search([('name', '=', 'web_responsive')]).button_immediate_install()
+    
+    env['ir.module.module'].search([('name', '=', 'website_horizon_responsive')]).button_immediate_install()
+    
+    env['ir.module.module'].search([('name', '=', 'letsencrypt')]).button_immediate_install()
             
 if __name__ == '__main__':
     main()
