46eb7561b69110db47d045773d72b57243724d46
Jérôme Sonnet <jerome.sonnet@imply.lu>
2022-01-06 15:09:06 +0100

    add fix-states.py

--- odoo/custom/src/private/fix-states.py
+++ odoo/custom/src/private/fix-states.py
@@ -7,15 +7,14 @@ import click_odoo
 @click.command()
 @click_odoo.env_options(default_log_level='error')
 def main(env):
-    for rec in env['school.individual_course_group'].search([]) :
-        if rec.valuated_program_id :
-            rec.state = '0_valuated'
-        elif rec.acquiered == 'A' :
-            rec.state = '6_success'
-        elif rec.acquiered == 'NA' and rec.final_result_bool :
-            rec.state = '7_failed'
-        else :
-            rec.state = '5_progress'
+    
+    env['school.individual_course_group'].search(['valuated_program_id','!=','False']).state = '0_valuated'
+    
+    env['school.individual_course_group'].search(['valuated_program_id','=','False'],['acquiered','=','A']).state = '6_success'
+    
+    env['school.individual_course_group'].search(['valuated_program_id','!=','False'],['acquiered','=','NA'],['final_result_bool','=','True']).state = '7_failed'
+    
+    env['school.individual_course_group'].search(['valuated_program_id','!=','False'],['acquiered','=','NA'],['final_result_bool','=','False']).state = '5_progress'
             
 if __name__ == '__main__':
     main()
