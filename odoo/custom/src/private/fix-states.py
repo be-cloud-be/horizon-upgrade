d06c88943c9ee5ace53aa0e8e12a7cd945e2667a
Jérôme Sonnet <jerome.sonnet@imply.lu>
2022-05-19 18:03:08 +0200

    fix fix state

--- odoo/custom/src/private/fix-states.py
+++ odoo/custom/src/private/fix-states.py
@@ -10,13 +10,13 @@ def main(env):
     
     env['school.individual_course_group'].search([('valuated_program_id','!=',False)]).state = '0_valuated'
     
-    env['school.individual_course_group'].search([('valuated_program_id','=',False),('acquiered','=','A'),('final_result_bool','=',False)]).state = '0_valuated'
+    env['school.individual_course_group'].search([('valuated_program_id','=',False),('acquiered','=','A'),('final_result_bool','=',False)]).write({'state':'0_valuated'})
     
-    env['school.individual_course_group'].search([('valuated_program_id','=',False),('acquiered','=','A')]).state = '6_success'
+    env['school.individual_course_group'].search([('valuated_program_id','=',False),('acquiered','=','A')]).write({'state':'6_success'})
     
-    env['school.individual_course_group'].search([('valuated_program_id','=',False),('acquiered','=','NA'),('final_result_bool','=',True)]).state = '7_failed'
+    env['school.individual_course_group'].search([('valuated_program_id','=',False),('acquiered','=','NA'),('final_result_bool','=',True)]).write({'state':'7_failed'})
 
-    env['school.individual_course_group'].search([('valuated_program_id','=',False),('acquiered','=','NA'),('final_result_bool','=',False)]).state = '5_progress'
+    env['school.individual_course_group'].search([('valuated_program_id','=',False),('acquiered','=','NA'),('final_result_bool','=',False)]).write({'state':'5_progress'})
             
 if __name__ == '__main__':
     main()
